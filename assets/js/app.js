a9b21e4ba4efa6960e5dc6966ba547ad620d035b
Cristian R <crisrodruiz@gmail.com>
2018-02-18 20:44:49 +0100

    Agregado assets, twig y plantilla base modificada

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+//Importa bootstrap y fontawesome
+import 'bootstrap';
+import fontawesome from '@fortawesome/fontawesome';
+import solid from '@fortawesome/fontawesome-free-solid'
+
+fontawesome.library.add(solid);

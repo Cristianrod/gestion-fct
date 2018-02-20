98856a4021983f12fe5f0ef6403f5737cb540bfb
Cristian R <crisrodruiz@gmail.com>
2018-02-20 19:46:08 +0100

    Agregado regex inicial en nif

--- src/Entity/Alumno.php
+++ src/Entity/Alumno.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\AlumnoRepository")
@@ -20,6 +21,8 @@ class Alumno
 
     /**
      * @ORM\Column(type="string")
+     * @Assert\Regex("/^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$/i")
+     * @Assert\NotBlank()
      */
     private $nif;
 

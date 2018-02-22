416f5df07389e9013d1c75e9efac76cd2b078411
Cristian R <crisrodruiz@gmail.com>
2018-02-22 20:13:16 +0100

    Modificacion en el controlador inicio

--- src/Controller/InicioController.php
+++ src/Controller/InicioController.php
@@ -7,7 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class IncioController extends Controller
+class InicioController extends Controller
 {
     /**
      * @Route("/", name="incio")
